1ce81b023354dc2d1b1146b0cd1c2299f919df50
ipsnaman <nsnamansingh94@gmail.com>
2024-10-18 13:01:14 +0530

    Add files via upload

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require jquery
+//= require popper
+//= require bootstrap-sprockets
+//= require chartkick
+//= require Chart.bundle
